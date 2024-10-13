c68dee7ec0f2976978c437e8e6bdccbdeefb6ebd
Marina Timofeeva <marina.timofeeva@emergingtravel.com>
2024-10-13 20:33:47 +0300

    Fix after review

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -23,51 +23,6 @@ type server struct {
 	desc.UnimplementedUserV1Server
 }
 
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("User name: %s", req.GetName())
-	log.Printf("User email: %s", req.GetEmail())
-	log.Printf("User role: %s", req.GetRole())
-
-	randomID, err := rand.Int(rand.Reader, big.NewInt(1<<63-1))
-	if err != nil {
-		return nil, err
-	}
-
-	log.Printf("User id: %d", randomID)
-
-	return &desc.CreateResponse{
-		Id: randomID.Int64(),
-	}, nil
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("User id: %d", req.GetId())
-
-	return &desc.GetResponse{
-		Id:        req.GetId(),
-		Name:      gofakeit.BeerName(),
-		Email:     gofakeit.IPv4Address(),
-		Role:      desc.Enum_Admin,
-		CreatedAt: timestamppb.New(gofakeit.Date()),
-		UpdatedAt: timestamppb.New(gofakeit.Date()),
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	log.Printf("User id: %d", req.GetId())
-	log.Printf("User name: %s", req.GetName())
-	log.Printf("User email: %s", req.GetEmail())
-	log.Printf("User role: %s", req.GetRole())
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("User id: %d", req.GetId())
-
-	return &emptypb.Empty{}, nil
-}
-
 func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
 	if err != nil {
@@ -83,4 +38,52 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
+}
+
+// Create - create new user
+func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf(
+		"User name: %s\n User email: %s\n User role: %s",
+		req.GetName(), req.GetEmail(), req.GetRole())
+
+	randomID, err := rand.Int(rand.Reader, big.NewInt(1<<63-1))
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("User id: %d", randomID)
+
+	return &desc.CreateResponse{
+		Id: randomID.Int64(),
+	}, nil
+}
+
+// Get - get user by id
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      gofakeit.BeerName(),
+		Email:     gofakeit.IPv4Address(),
+		Role:      desc.Enum_ADMIN,
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
+// Update - update user by id
+func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("User id: %d", req.GetId())
+	log.Printf("User name: %s", req.GetName())
+	log.Printf("User role: %s", req.GetRole())
+
+	return &emptypb.Empty{}, nil
+}
+
+// Delete - delete user by id
+func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
 }
