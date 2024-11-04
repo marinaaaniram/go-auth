013810714f12e870011fc2c517484a23688ddb29
Marina Timofeeva <marina.timofeeva@emergingtravel.com>
2024-11-04 13:39:43 +0300

    Add passwords check

--- internal/api/user/create.go
+++ internal/api/user/create.go
@@ -5,9 +5,15 @@ import (
 
 	"github.com/marinaaaniram/go-auth/internal/converter"
 	desc "github.com/marinaaaniram/go-auth/pkg/user_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (i *Implementation) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	if req.Password != req.PasswordConfirm {
+		return nil, status.Error(codes.InvalidArgument, "'password' and 'password_confirm' do not match")
+	}
+
 	userDesc, err := i.userService.Create(ctx, converter.FromDescCreateToUser(req))
 	if err != nil {
 		return nil, err
