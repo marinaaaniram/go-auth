4b143757562a68a76dbc602593c1df60f71f0f69
Marina Timofeeva <marina.timofeeva@emergingtravel.com>
2024-11-10 20:36:45 +0300

    Implemented api tests

---
+++ internal/api/user/tests/delete_test.go
@@ -0,0 +1,113 @@
+package tests
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/gojuno/minimock/v3"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"github.com/marinaaaniram/go-auth/internal/api/user"
+	"github.com/marinaaaniram/go-auth/internal/errors"
+	"github.com/marinaaaniram/go-auth/internal/model"
+	"github.com/marinaaaniram/go-auth/internal/service"
+	serviceMocks "github.com/marinaaaniram/go-auth/internal/service/mocks"
+	desc "github.com/marinaaaniram/go-auth/pkg/user_v1"
+)
+
+func TestApiUserDelete(t *testing.T) {
+	t.Parallel()
+	type userServiceMockFunc func(mc *minimock.Controller) service.UserService
+
+	type args struct {
+		ctx context.Context
+		req *desc.DeleteRequest
+	}
+
+	var (
+		ctx = context.Background()
+		mc  = minimock.NewController(t)
+
+		id = gofakeit.Int64()
+
+		serviceErr = fmt.Errorf("Service error")
+
+		serviceReq = &model.User{
+			ID: id,
+		}
+
+		req = &desc.DeleteRequest{
+			Id: id,
+		}
+
+		res = &emptypb.Empty{}
+	)
+
+	defer t.Cleanup(mc.Finish)
+
+	tests := []struct {
+		name            string
+		args            args
+		want            *emptypb.Empty
+		err             error
+		userServiceMock userServiceMockFunc
+	}{
+		{
+			name: "Success case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: res,
+			err:  nil,
+			userServiceMock: func(mc *minimock.Controller) service.UserService {
+				mock := serviceMocks.NewUserServiceMock(mc)
+				mock.DeleteMock.Expect(ctx, serviceReq).Return(nil)
+				return mock
+			},
+		},
+		{
+			name: "Api nil pointer",
+			args: args{
+				ctx: ctx,
+				req: nil,
+			},
+			want: nil,
+			err:  errors.ErrPointerIsNil("req"),
+			userServiceMock: func(mc *minimock.Controller) service.UserService {
+				return nil
+			},
+		},
+		{
+			name: "Service error case",
+			args: args{
+				ctx: ctx,
+				req: req,
+			},
+			want: nil,
+			err:  serviceErr,
+			userServiceMock: func(mc *minimock.Controller) service.UserService {
+				mock := serviceMocks.NewUserServiceMock(mc)
+				mock.DeleteMock.Expect(ctx, serviceReq).Return(serviceErr)
+				return mock
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			userServiceMock := tt.userServiceMock(mc)
+			api := user.NewUserImplementation(userServiceMock)
+
+			newID, err := api.Delete(tt.args.ctx, tt.args.req)
+			require.Equal(t, tt.err, err)
+			require.Equal(t, tt.want, newID)
+		})
+	}
+}
